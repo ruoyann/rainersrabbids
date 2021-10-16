dd72254104ecf27c9b0a458463951e276a00e94a
hwyiing <pics.cbj@gmail.com>
2021-10-16 15:18:08 +0800

    null model

---
+++ rainer_rabbids/lib/widgets/alphabet_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AlphabetCard extends StatelessWidget {
+  const AlphabetCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+    );
+  }
+}
